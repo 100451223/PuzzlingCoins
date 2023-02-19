0fe5d38d3fe7ffc4b53c3a495ed4ee2a7fbfa4b3
Daniel <100451223@alumnos.uc3m.es>
2023-02-19 16:15:00 +0100

    Added coin increment functionality

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -62,3 +62,71 @@ function addCoinAudio(){
     document.body.appendChild(audio);
     return audio
 }
+
+function setWealthImage(coinsAmount){
+    /* Set the image for the wealth notification */
+
+    //console.log(coinsAmount)
+    let wealthImage = "../images/wealth/";
+
+    if (coinsAmount>=0 && coinsAmount<=30)
+        wealthImage += "001_030.png";
+    else if (coinsAmount>=31 && coinsAmount<=50)
+        wealthImage += "031_050.png";
+    else if (coinsAmount>=51 && coinsAmount<=80)
+        wealthImage += "051_080.png";
+    else if (coinsAmount>=81 && coinsAmount<=150)
+        wealthImage += "081_150.png";
+    else if (coinsAmount>=151 && coinsAmount<=229)
+        wealthImage += "151_229.png";
+    else if (coinsAmount>=230)
+        wealthImage += "230_inf.png";
+
+    return wealthImage;
+}
+
+function showWealth(amount){
+    /* Show the user how much money they have in a notification*/
+
+    wealthImage = setWealthImage(amount)
+
+    let outerDiv = document.createElement("div");
+    outerDiv.className = "coinAmount";
+
+    let innerImg = document.createElement("img");
+    innerImg.src = chrome.extension.getURL(wealthImage);
+
+    let innerTxt = document.createElement("p");
+    innerTxt.id = "coinAmountTxt";
+    innerTxt.innerText = "You have " +amount + " coins";
+
+    outerDiv.appendChild(innerImg);
+    outerDiv.appendChild(innerTxt);
+
+    document.body.appendChild(outerDiv);
+    setTimeout(()=>outerDiv.remove(), 5000);
+
+}
+
+function initialSetUp(){
+    /* Set up the local storage object for the user */
+
+    chrome.storage.local.get(["usersCase"], (result) => {
+        if (result.usersCase == undefined)
+        chrome.storage.local.set({usersCase: {coinsAmount: 0, todayIs: new Date().getDate(), coinsToday: 0}});
+        });
+}
+
+function incrementUsersCoinAmount(){
+    /* Increment the amount of coins the user has */
+
+    chrome.storage.local.get(["usersCase"], ((result) => {
+        let newAmount = result.usersCase;
+        newAmount.coinsAmount += 1;
+        chrome.storage.local.set({usersCase: newAmount}, ()=>{
+        console.log("Value is set to " + newAmount.coinsAmount)
+        showWealth(parseInt(newAmount.coinsAmount));
+        });
+    }))
+
+}
